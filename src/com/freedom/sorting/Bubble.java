870a24b649b6a4cc5f5e7b91f7ec5bf5c4e985ab
Sun <shloveqls@gmail.com>
2016-07-28 17:48:48 +0900

    add sorting algorithm

--- src/com/freedom/sorting/Bubble.java
+++ src/com/freedom/sorting/Bubble.java
@@ -2,6 +2,34 @@ package com.freedom.sorting;
 
 public class Bubble {
 
+    public static void main(String args[]) {
 
+        Integer[] numbers = {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
+
+        int count = 0;
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for (int i = 0; i < numbers.length - 1; i++) {
+                if (numbers[i] > numbers[i + 1]) {
+                    // swap(leftElement, rightElement)
+                    Integer temp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = temp;
+                    swapped = true;
+                    count++;
+                }
+            }
+        } while (swapped);
+
+        print(numbers, count);
+
+    }
+
+    private static void print(Integer[] numbers, int count) {
+        Utilities.printArray(numbers);
+        System.out.println("Inversion Count: " + count);
+    }
 
 }
---
+++ src/com/freedom/sorting/Merge.java
@@ -0,0 +1,9 @@
+package com.freedom.sorting;
+
+public class Merge {
+
+    public static void main(String args[]) {
+
+    }
+
+}
